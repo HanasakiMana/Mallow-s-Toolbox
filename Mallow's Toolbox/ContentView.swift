a27c5e273e9632b3eab2fcef059964cf599bedc7
Mallow <18103235+mallow5359@users.noreply.github.com>
2022-04-26 02:41:52 +0800

    initial commit

---
+++ Mallow's Toolbox/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  Mallow's Toolbox
+//
+//  Created by Mallow on 2022/2/23.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ Mallow's Toolbox/Mallow_s_ToolboxApp.swift
@@ -0,0 +1,17 @@
+//
+//  Mallow_s_ToolboxApp.swift
+//  Mallow's Toolbox
+//
+//  Created by Mallow on 2022/2/23.
+//
+
+import SwiftUI
+
+@main
+struct Mallow_s_ToolboxApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
